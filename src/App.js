ac9c2e8ee50e442a1e1847cad4c6f89627ab405a
Fasuto1st <nicha.tnct@gmail.com>
2023-03-08 18:49:37 +0700

    Up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Chart from 'react-apexcharts'
+import Navbar from './Navbar';
 
 function App() {
+  const [options, setOptions] = useState({
+    chart: {
+      type: 'bar'
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995]
+    }
+  })
+  const [series, setSeries] = useState([
+    {
+      name: 'a',
+      data: [10, 20, 30, 40, 50]
+    },
+    {
+      name: 'b',
+      data: [55, 45, 35, 25, 15]
+    }
+  ])
+
+  useEffect(() => {
+    const tDate = new Date()
+    const tDay = tDate.getDate() - 7
+    tDate.setDate(tDay)
+    const startDate = tDate.toISOString().slice(0,10)
+    document.getElementById("date").innerHTML = startDate
+    alert(startDate)
+    fetch('https://www.melivecode.com/api/pets/7days/' + startDate)
+      .then(res => res.json())
+      .then((result) => {
+        setOptions({
+          ...options,
+          xaxis: {
+            categories: result.categories
+          }
+        })
+        // setSeries(result.series)
+        setSeries(result.series)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <p id ="date" align="center"></p>
+      <Chart
+        options={options}
+        series={series}
+        type='bar'
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
